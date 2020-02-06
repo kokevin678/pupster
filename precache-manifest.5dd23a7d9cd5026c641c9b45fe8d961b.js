05d4e7f2f821e444afb0f3ee1c2570701a776627
Chengyu Ko <kokevin678@gmail.com>
2020-02-06 11:44:28 -0800

    Updates

---
+++ precache-manifest.5dd23a7d9cd5026c641c9b45fe8d961b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1f7da8787122606cde236bccf21bb9",
+    "url": "/pupster/index.html"
+  },
+  {
+    "revision": "b6d6fbc8d8865a9ca129",
+    "url": "/pupster/static/css/main.97f1e7cb.chunk.css"
+  },
+  {
+    "revision": "b47d12212ad473c022b0",
+    "url": "/pupster/static/js/2.0a5a0513.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pupster/static/js/2.0a5a0513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d6fbc8d8865a9ca129",
+    "url": "/pupster/static/js/main.4f92bd6a.chunk.js"
+  },
+  {
+    "revision": "5fceed75274683a4a3f6",
+    "url": "/pupster/static/js/runtime-main.0977139c.js"
+  }
+]);
